fcfdbdcdb3027d9f8c4d4e1449d2c77ec8d93ff3
Roberto Paternina <paternina.dev@gmail.com>
2023-09-15 19:01:11 -0500

    new ver

--- inv.js
+++ inv.js
@@ -24,34 +24,11 @@ document.addEventListener("DOMContentLoaded", () => {
                 <h1>¡Te esperamos!</h1>
                 <h2>Luna Camacho Fernandez</h2>
               </div>
-            <div class="footer">                
-                <button class="default" id="destruir">Destruir</button>
-            </div>
         `;
 
         card.classList.remove("explosion");
 
         const destruir = document.getElementById("destruir");
-
-        destruir.addEventListener(
-          "click",
-          () => {
-            card.classList.remove("revealed");
-            card.classList.add("explosion");
-            card.classList.add("destruida");
-
-            setTimeout(() => {
-              card.innerHTML = `
-                    <h1 class="title">Mensaje destruido</h1>
-                    <p>¡Ya no puedes verme más!</p>
-                    <button id="revelar" disabled>¡Bloqueado!</button>
-                `;
-
-              card.classList.remove("explosion");
-            }, 500);
-          },
-          { once: true }
-        );
       }, 500);
     },
     { once: true }
